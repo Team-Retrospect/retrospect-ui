b1906d4d3b7cfb3c7954283dc6556b894ac9f9e7
Eufrosino <eufrosinoveloso@gmail.com>
2021-07-14 08:38:53 -0500

    Added ability to search spans using new go-cassandra api endpoint

--- client/src/components/SpanSearch.js
+++ client/src/components/SpanSearch.js
@@ -1,13 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import Span from './Span';
+import SpanSearchForm from './SpanSearchForm';
 import axios from 'axios';
 
 const SpanSearch = () => {
 	const [spans, setSpans] = useState([]);
+	const [search, setSearch] = useState(false);
+	const [traceId, setTraceId] = useState('');
+	const [userId, setUserId] = useState('');
+	const [sessionId, setSessionId] = useState('');
+	const [chapterId, setChapterId] = useState('');
+	const [statusCode, setStatusCode] = useState('');
+
+	const values = {
+		traceId, 
+		userId, 
+		sessionId, 
+		chapterId, 
+		statusCode, 
+	}
+
+	const setFunctions = {
+		setTraceId, 
+		setUserId, 
+		setSessionId, 
+		setChapterId, 
+		setStatusCode, 
+	}
 
 	useEffect(() => {
 		let search = {
-			trace_id: '49925f0ba546eec93d4dfda392664414',
+			trace_id: traceId,
+			user_id: userId,
+			session_id: sessionId,
+			chapter_id: chapterId,
+			status_code: statusCode,
 		}
 
 		let queryString = [];
@@ -19,12 +46,19 @@ const SpanSearch = () => {
 		axios
 			.get(`/api/span_search?${queryStringConcat}`)
       .then((response) => {
+				console.log('hit api')
 				setSpans(response.data)
 			})
-	}, []);
+	}, [search]);
+
+	const handleSearch = () => {
+		setSearch(!search);
+	}
 
 	return (
 		<div>
+			<SpanSearchForm values={values} setFunctions={setFunctions} />
+			<button class="btn btn-primary" onClick={handleSearch}>Apply Search</button>
 			<div id="span-list">
 				{spans.map((span) => {
 					return <Span key={span.span_id} span={span} />;
