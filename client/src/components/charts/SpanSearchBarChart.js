8c27891a3a3bd0f926213d4eb85b0a72211af922
sb916 <50715937+svetlanabrennan@users.noreply.github.com>
2021-07-26 18:24:48 -0500

    Fixed top chart in chapter page

--- client/src/components/charts/SpanSearchBarChart.js
+++ client/src/components/charts/SpanSearchBarChart.js
@@ -1,17 +1,29 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
+import Chart from 'chart.js/auto'
 import generateBarChartData from './generateBarChartData';
-import BarChart from './BarChart'
+
+let myChart;
 
 const SpanSearchBarChart = ({ spans }) => {
-  console.log("passed spans in Span Search", spans)
+  const chartRef = useRef(null);
 
 	const spanData = generateBarChartData(spans)
 	const spanOptions = {
 	  indexAxis: 'y'
 	};
 
+  useEffect(() => {
+    if (typeof myChart !== "undefined") myChart.destroy();
+
+    myChart = new Chart(chartRef.current, {
+      type: 'bar',
+      data: spanData,
+      options: spanOptions,
+    })
+  }, [spans])
+
   return (
-    <BarChart data={spanData} options={spanOptions} />
+    <canvas ref={chartRef} />
   )
 }
 
